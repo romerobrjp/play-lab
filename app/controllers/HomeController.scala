bec410b55487b5e4c38d09105dda9903e2a549d2
romeromedeiros <romeromedeiros5@gmail.com>
2018-01-25 15:45:56 -0300

    Listar participantes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,29 @@
 package controllers
 
-import javax.inject._
-import play.api._
 import play.api.mvc._
+
+import scala.collection.mutable.{ListBuffer}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
-class HomeController @Inject() extends Controller {
+class HomeController extends Controller {
+  val participantes: ListBuffer[Participante] = ListBuffer(
+    Participante("Romero", "romero@gmail.com"),
+    Participante("Claudino", "claudino@gmail.com"),
+    Participante("Mauricio", "mauricio@gmail.com")
+  )
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def listarParticipantes = Action {
+    Ok(participantes.toString())
+  }
+
+  def novoParticipante = TODO
 }
+
+case class Participante(nome: String, email: String)
