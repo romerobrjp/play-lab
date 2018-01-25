ac075879aaad7d1c7a5b133998b5864731dd958b
romeromedeiros <romeromedeiros5@gmail.com>
2018-01-25 17:07:31 -0300

    Lab 3.3 (sessions)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -15,12 +15,18 @@ class HomeController extends Controller {
     Participante("Mauricio", "mauricio@gmail.com")
   )
 
+  var visitas: Int = 1
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def listarParticipantes = Action {
-    Ok(participantes.toString())
+  def listarParticipantes = Action { request =>
+    val visitas = request.session.get("visitas")
+    .map(v => v.toInt + 1)
+    .getOrElse(1)
+
+    Ok(s"Numero de visitas: $visitas \n${participantes.mkString("-", "\n-", "\n")}").withSession("visitas" -> visitas.toString)
   }
 
   def novoParticipante = TODO
